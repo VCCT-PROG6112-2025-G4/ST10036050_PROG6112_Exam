b3d643f9521330e900faf16af6f00f447a74afc8
dubem4521@gmail.com <st10036050@vcconnect.edu.za>
2025-11-10 11:30:47 +0200

    Created my 3 required classes for question 1

---
+++ src/st10036050_prog6112_exam/ProductSales.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package st10036050_prog6112_exam;
+
+/**
+ *
+ * @author maxwe
+ */
+public class ProductSales {
+    
+}
--- src/st10036050_prog6112_exam/ST10036050_PROG6112_Exam.java
+++ src/st10036050_prog6112_exam/ST10036050_PROG6112_Exam.java
@@ -14,7 +14,16 @@ public class ST10036050_PROG6112_Exam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Initialize the 2D array with product sales data
+        int[][] productSales = {
+            {300, 150, 700},  // Year 1: Quarter 1, Quarter 2, Quarter 3
+            {250, 200, 600}   // Year 2: Quarter 1, Quarter 2, Quarter 3
+        };
+        
+        
+        
+        
+        
+        
     }
-    
 }
